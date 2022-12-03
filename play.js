6d82c3add49008c245504f398c69198f57024a71
tada <rortheford@gmail.com>
2022-12-03 11:38:57 -0800

    Convert computer move from number to readable move of rps

--- play.js
+++ play.js
@@ -1,3 +1,9 @@
 function getComputerChoice() {
-    return parseInt(Math.floor(Math.random() * 3) + 1);
+    random = parseInt(Math.floor(Math.random() * 3) + 1);
+    if (random == 1) 
+        return "ROCK";
+    else if (random == 2) 
+        return "PAPER";
+    else
+        return "SCISSORS";
 }

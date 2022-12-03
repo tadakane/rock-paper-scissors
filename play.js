cd8fdb54b588adb8be58d5af5e74692ca051e429
tada <rortheford@gmail.com>
2022-12-03 12:44:51 -0800

    Change playRound function to return integer based on results

--- play.js
+++ play.js
@@ -10,28 +10,47 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() == "ROCK") {
-        if (computerSelection == "ROCK")
-            return "It's a tie! Rock ties Rock";
-        else if (computerSelection == "PAPER")
-            return "You lose! Paper beats Rock";
-        else    
-            return "You win! Rock beats Scissors";
+        if (computerSelection == "ROCK") {
+            console.log("It's a tie! Rock ties Rock");
+            return 0;
+        }
+        else if (computerSelection == "PAPER") {
+            console.log("You lose! Paper beats Rock");
+            return -1;
+        }
+        else {
+            console.log("You win! Rock beats Scissors");
+            return 1;
+        }
     }
     else if (playerSelection.toUpperCase() == "PAPER") {
-        if (computerSelection == "ROCK")
-            return "You win! Paper beats Rock";
-        else if (computerSelection == "PAPER")
-            return "It's a tie! Paper ties Paper";
-        else    
-            return "You lose! Scissors beats Paper";
+        if (computerSelection == "ROCK") {
+            console.log("You win! Paper beats Rock");
+            return 1;
+        }
+        else if (computerSelection == "PAPER") {
+            console.log("It's a tie! Paper ties Paper");
+            return 0;
+        }
+        else {
+            console.log("You lose! Scissors beats Paper");
+            return -1;
+        }
     }
-    else
-        if (computerSelection == "ROCK")
-            return "You lose! Rock beats Scissors";
-        else if (computerSelection == "PAPER")
-            return "You win! Scissors beats Paper";
-        else
-            return "It's a tie! Scissors ties Scissors";
+    else {
+        if (computerSelection == "ROCK") {
+            console.log("You lose! Rock beats Scissors");
+            return -1;
+        }
+        else if (computerSelection == "PAPER") {
+            console.log("You win! Scissors beats Paper");
+            return 1;
+        }
+        else {
+            console.log("It's a tie! Scissors ties Scissors");
+            return 0;
+        }
+    }
 }
 
 function game() {

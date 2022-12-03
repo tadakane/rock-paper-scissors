6ce9d797acbd2c7b55d19d794ee823f16e51ee8e
tada <rortheford@gmail.com>
2022-12-03 12:23:54 -0800

    Change console log of round results to function return

--- play.js
+++ play.js
@@ -11,25 +11,26 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() == "ROCK") {
         if (computerSelection == "ROCK")
-            console.log("It's a tie! Rock ties Rock");
+            return "It's a tie! Rock ties Rock";
         else if (computerSelection == "PAPER")
-            console.log("You lose! Paper beats Rock");
+            return "You lose! Paper beats Rock";
         else    
-            console.log("You win! Rock beats Scissors");
+            return "You win! Rock beats Scissors";
     }
     else if (playerSelection.toUpperCase() == "PAPER") {
         if (computerSelection == "ROCK")
-            console.log("You win! Paper beats Rock");
+            return "You win! Paper beats Rock";
         else if (computerSelection == "PAPER")
-            console.log("It's a tie! Paper ties Paper");
+            return "It's a tie! Paper ties Paper";
         else    
-            console.log("You lose! Scissors beats Paper");
+            return "You lose! Scissors beats Paper";
     }
     else
         if (computerSelection == "ROCK")
-            console.log("You lose! Rock beats Scissors");
+            return "You lose! Rock beats Scissors";
         else if (computerSelection == "PAPER")
-            console.log("You win! Scissors beats Paper");
+            return "You win! Scissors beats Paper";
         else
-            console.log("It's a tie! Scissors ties Scissors");
+            return "It's a tie! Scissors ties Scissors";
 }
+
